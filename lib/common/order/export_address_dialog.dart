60b71b518a7c796a2b2924df2693e658d89fab6d
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-01 10:54:02 -0400

    Implement save screenshot of address to gallery

--- lib/common/order/export_address_dialog.dart
+++ lib/common/order/export_address_dialog.dart
@@ -1,26 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:loja_virtual/models/address.dart';
+import 'package:screenshot/screenshot.dart';
 
 class ExportAddressDialog extends StatelessWidget {
   final Address address;
-  const ExportAddressDialog({Key? key, required this.address})
-      : super(key: key);
+  final ScreenshotController _screenshotController = ScreenshotController();
+  ExportAddressDialog({Key? key, required this.address}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      contentPadding: EdgeInsets.fromLTRB(16, 16, 16, 0),
-      title: Text('Endereço de Entrega'),
-      content: Text(
-        '${address.street}, ${address.number} ${address.complement}\n'
-        '${address.district}\n'
-        '${address.city}/${address.state}\n'
-        '${address.zipCode}',
+      contentPadding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+      title: const Text('Endereço de Entrega'),
+      content: Screenshot(
+        controller: _screenshotController,
+        child: Container(
+          color: Colors.white,
+          padding: EdgeInsets.all(8),
+          child: Text(
+            '${address.street}, ${address.number} ${address.complement}\n'
+            '${address.district}\n'
+            '${address.city}/${address.state}\n'
+            '${address.zipCode}',
+          ),
+        ),
       ),
       actions: [
         TextButton(
-          onPressed: () {},
-          child: Text('Exportar'),
+          onPressed: () async {
+            Navigator.of(context).pop();
+            final imgAsBytes = await _screenshotController.capture();
+            await ImageGallerySaver.saveImage(imgAsBytes!);
+          },
+          style: TextButton.styleFrom(
+            foregroundColor: Theme.of(context).primaryColor,
+          ),
+          child: const Text('Exportar'),
         ),
       ],
     );
