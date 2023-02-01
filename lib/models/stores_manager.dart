a8ad42fa01a1ef0179c69bb905bc611962c3cf07
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-01 16:46:34 -0400

    Create store object and special constructor

--- lib/models/stores_manager.dart
+++ lib/models/stores_manager.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/store.dart';
 
 class StoresManager extends ChangeNotifier {
+  List<Store> stores = [];
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   StoresManager() {
@@ -10,7 +12,8 @@ class StoresManager extends ChangeNotifier {
 
   Future<void> _loadStoreList() async {
     final snapshot = await _firestore.collection('stores').get();
-    print(snapshot.docs.first.data());
+    stores =
+        snapshot.docs.map((storeDoc) => Store.fromDocument(storeDoc)).toList();
     notifyListeners();
   }
 }
