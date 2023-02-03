f193468c1d0cb09e92293304b521333982400d11
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-03 10:37:06 -0400

    Implement card back textfield and validation

--- lib/screens/checkout/components/card_back.dart
+++ lib/screens/checkout/components/card_back.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:loja_virtual/screens/checkout/components/card_text_field.dart';
 
 class CardBack extends StatelessWidget {
   const CardBack({Key? key}) : super(key: key);
@@ -14,6 +16,48 @@ class CardBack extends StatelessWidget {
       child: Container(
         height: 200,
         color: const Color(0xff1B4B52),
+        child: Column(
+          children: [
+            Container(
+              color: Colors.black,
+              height: 40,
+              margin: const EdgeInsets.symmetric(vertical: 16),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 70,
+                  child: Container(
+                    alignment: Alignment.centerRight,
+                    color: Colors.grey[500],
+                    margin: const EdgeInsets.only(left: 12),
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                    child: CardTextField(
+                      hint: '123',
+                      maxLength: 3,
+                      textAlign: TextAlign.end,
+                      textInputType: TextInputType.number,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                      ],
+                      validator: (cvv) {
+                        if (cvv == null || cvv.length != 3)
+                          return 'Inválido';
+                        else
+                          return null;
+                      },
+                    ),
+                  ),
+                ),
+                const Expanded(
+                  flex: 30,
+                  child: SizedBox(),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/screens/checkout/components/card_text_field.dart
+++ lib/screens/checkout/components/card_text_field.dart
@@ -2,20 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class CardTextField extends StatelessWidget {
-  final String title;
+  final String? title;
   final bool bold;
   final String hint;
   final TextInputType textInputType;
   final List<TextInputFormatter>? inputFormatters;
   final FormFieldValidator<String> validator;
+  final int? maxLength;
+  final TextAlign textAlign;
   const CardTextField({
     Key? key,
-    required this.title,
+    this.title,
     this.bold = false,
     required this.hint,
     this.textInputType = TextInputType.text,
     this.inputFormatters,
     required this.validator,
+    this.maxLength,
+    this.textAlign = TextAlign.start,
   }) : super(key: key);
 
   @override
@@ -27,41 +31,49 @@ class CardTextField extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              children: [
-                Text(
-                  title,
-                  style: const TextStyle(
-                    fontSize: 10,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
-                  ),
-                ),
-                if (state.hasError)
-                  const Text(
-                    '    Inválido',
-                    style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 9,
+            if (title != null)
+              Row(
+                children: [
+                  Text(
+                    title!,
+                    style: const TextStyle(
+                      fontSize: 10,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
                     ),
-                  )
-              ],
-            ),
+                  ),
+                  if (state.hasError)
+                    const Text(
+                      '    Inválido',
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 9,
+                      ),
+                    )
+                ],
+              ),
             TextFormField(
               style: TextStyle(
-                color: Colors.white,
+                color:
+                    title == null && state.hasError ? Colors.red : Colors.white,
                 fontWeight: bold ? FontWeight.bold : FontWeight.w500,
               ),
               decoration: InputDecoration(
                 hintText: hint,
                 hintStyle: TextStyle(
-                  color: Colors.white.withAlpha(100),
+                  color: title == null && state.hasError
+                      ? Colors.red.withAlpha(200)
+                      : Colors.white.withAlpha(100),
                 ),
                 border: InputBorder.none,
                 isDense: true,
                 contentPadding: const EdgeInsets.symmetric(vertical: 2),
+                counterText: '',
               ),
+              cursorColor: Colors.white,
+              maxLength: maxLength,
               keyboardType: textInputType,
+              textAlign: textAlign,
               inputFormatters: inputFormatters,
               onChanged: (text) => state.didChange(text),
             ),
